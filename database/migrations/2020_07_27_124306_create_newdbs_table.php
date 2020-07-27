0459f12b92a8ad0dfc6a9b37dcb5889b1b33c97d
Liou <a8532582@gmail.com>
2020-07-27 21:11:38 +0800

    addnewdbAndSelectAll

---
+++ database/migrations/2020_07_27_124306_create_newdbs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewdbsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newdbs', function (Blueprint $table) {
+            $table->id();
+            $table->char('status');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newdbs');
+    }
+}
